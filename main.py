e9cc26c9f35618cf9c89138aa06a829027484bed
FredHobel <31831984+FredHobel@users.noreply.github.com>
2020-08-19 14:23:40 -0700

    Update main.py
    made the plot 3d to better see the trajectories

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 plt.style.use('seaborn-white')
 import random as rn
 
@@ -14,7 +15,7 @@ def ddy(function, x, y, t, h):
 
 def fieldStrength(positionx, positiony, time):
     #return 10*np.sin(positionx*10 - 10*time*dt) + 10*positionx*np.cos(positiony*10 + 10*time*dt)
-    return (positionx) ** 4 + (positiony) ** 4
+    return (positionx) ** 4 + (positiony) ** 4 - (positionx) ** 2 - (positiony) ** 2
 
 
 def interactx(part1,part2):
@@ -75,7 +76,7 @@ class particle:
 
 p1 = particle(0, 0, rn.uniform(-0.2,0.2), rn.uniform(-0.2,0.2), 1, 10, 0, 0)
 p2 = particle(rn.uniform(-0.5,0.5), rn.uniform(-0.5,0.5), rn.uniform(-0.1,0.1), rn.uniform(-0.1,0.1), 1, rn.uniform(1,100), 0, 0)
-p3 = particle(rn.uniform(-0.5,0.5), rn.uniform(-0.5,0.5), rn.uniform(-0.1,0.1), rn.uniform(-0.1,0.1), 1, rn.uniform(1,100), 0, 0)
+p3 = particle(rn.uniform(-0.5,0.5), rn.uniform(-0.5,0.5), rn.uniform(-0.1,0.1), rn.uniform(-0.1,0.1), -1, rn.uniform(1,100), 0, 0)
 
 x1 = np.zeros(10000)
 y1 = np.zeros(10000)
@@ -85,6 +86,8 @@ y2 = np.zeros(10000)
 
 x3 = np.zeros(10000)
 y3 = np.zeros(10000)
+
+t = np.zeros(10000)
 
 for i in range(0,10000):
     p1.time = i
@@ -114,6 +117,8 @@ for i in range(0,10000):
     x3[i] = p3.x
     y3[i] = p3.y
 
+    t[i] = i
+
 gridx = np.linspace(-10, 10, 100)
 gridy = np.linspace(-10, 10, 100)
 
@@ -122,15 +127,21 @@ C = fieldStrength(A, B, 0)
 plt.contour(A, B, C, colors = 'black')
 plt.show()
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot(x1, y1, t, 'y')
+ax.plot(x2, y2, t, 'r')
+ax.plot(x3, y3, t, 'g')
 
 
-plt.plot(x1, y1, 'bo')
-plt.plot(x2, y2, 'ro')
-plt.plot(x3, y3, 'go')
+#plt.plot(x1, y1, 'yo')
+#plt.plot(x2, y2, 'ro')
+#plt.plot(x3, y3, 'go')
 
-plt.plot(x1[0], y1[0], 'ko')
-plt.plot(x2[0], y2[0], 'ko')
-plt.plot(x3[0], y3[0], 'ko')
+#plt.plot(x1[0], y1[0], 'ko')
+#plt.plot(x2[0], y2[0], 'ko')
+#plt.plot(x3[0], y3[0], 'ko')
 plt.show()
 
 
